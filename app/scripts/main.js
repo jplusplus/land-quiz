3b7253763cbb91ea36ffc8f1609cd1c361d76ee5
Ricardo Lafuente <r@manufacturaindependente.org>
2015-10-20 00:17:45 +0200

    Finish quiz logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,61 +7,69 @@ var answerLog = {};
 var currentQuestionId;
 
 function updateQuestion (q) {
-  currentQuestionId = q.id;
+  currentQuestionId = parseInt(q.id);
   // Update answer counter
   $('#question-current').text(Object.keys(answerLog).length + 1);
   // Fill the elements with the new content
-  $('#question-title').text(q.title);
+  $('#question-title').text(q.question);
   $('#question-explanation').html(marked(q.explanation));
 
   // create and populate answer list
   $('#answers').empty();
   $.each(q.answers, function(index, v) {
+    var $li = $('<li></li>').attr('id', 'answer-' + index);
     $('<a>', {
-      id: 'answer-' + index,
       class: 'select left-icon button split medium secondary no-pip',
     })
     .html('<span></span> ' + v)
-    .wrapInner('<li>')
-    .appendTo('#answers');
+    .appendTo($li);
+    $li.appendTo('#answers');
   });
+  // set up answer click callbacks
+  $('#answers li').click( function () {
+    $('#answers li').removeClass('selected');
+    $(this).toggleClass('selected');
+  });
+
 }
+
 
 $(document).ready(function() {    
   var jsonPath = '/data/questions.json';
   $.getJSON( jsonPath, function( data ) {
-    // go through all questions and store them in the array
-    $.each( data, function( key, c ) {
-      var q = {};
-      q.id = c.id;
-      q.title = c.question;
-      q.explanation = c.explanation;
-      q.answers = c.answers;
-      questions.push(q);
+    // Go through all questions and store them in the array
+    $.each(data, function( key, c ) {
+      questions.push(c);
     });
     $('#questions-total').text = questions.length;
     $('#questions-current').text = "1";
     updateQuestion(questions[0]);
   });
+  $('#next-button').click( function () {
+    if ($('#answers li.selected').length == 0) {
+      // Make sure we have a selected answer before going on
+      return
+    }
+    // Record the current answer 
+    answerLog[currentQuestionId] = $('#answers li.selected')[0].id.replace('answer-', '');
+    console.log(answerLog);
+    // Remove the last question from the questions array and present the next one
+    questions.shift();
+    var q = questions[0];
+    if (typeof q != 'undefined') {
+      // next question
+      updateQuestion(q);
+    } else {
+      // quiz finished
+      var posting = $.post("http://dialektapi.jplusplus.se/oracle/predict/", answerLog);
+      posting.done(function(data) {
+          console.log("Data Loaded: ");
+          console.log(data);
+      });
+    }
+  });
 });
 
-$('#next-button').click( function () {
-  // TODO: Make sure we have a selected answer before going on
-  
-  // Record the current answer 
-  answerLog[currentQuestionId] = "0";
-  console.log(answerLog);
-
-  // Remove last question from the questions array and present the next one
-  questions.shift();
-  var q = questions[0];
-  if (typeof q != 'undefined') {
-    updateQuestion(q);
-  } else {
-    console.log("Game over!");
-  }
-
-});
 
 // init Foundation
-// $(document).foundation();
+$(document).foundation();
